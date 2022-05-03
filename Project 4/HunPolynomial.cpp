3ccfc2a68aac402f97caf5ed897c702283b48517
JukeDuke <masterofegy@gmail.com>
2022-05-03 03:28:01 -0400

    finalized

---
+++ Project 4/HunPolynomial.cpp
@@ -0,0 +1,169 @@
+#include "HunPolynomial.h"
+
+HunPolynomial::HunPolynomial() : coefficiants({}) {}
+
+HunPolynomial::HunPolynomial(const vector<int>& coefficiants)
+{
+    vector<int> trimmedCoefficiants;
+    bool leadingZero = true;
+
+    for (int i = 0; i < coefficiants.size(); ++i)
+    {
+        if (coefficiants[i] != 0)
+        {
+            leadingZero = false;
+            trimmedCoefficiants.push_back(coefficiants[i]);
+        }
+        else if (!leadingZero)
+            trimmedCoefficiants.push_back(coefficiants[i]);
+    }
+
+    this->coefficiants = trimmedCoefficiants;
+}
+
+void HunPolynomial::Set(const vector<int>& coefficiants) { this->coefficiants = coefficiants; }
+
+HunPolynomial HunPolynomial::operator+(const HunPolynomial& other) const
+{
+    stack<int> result;
+    int i = coefficiants.size() - 1, j = other.coefficiants.size() - 1;
+
+    while (i >= 0 || j >= 0)
+    {
+        int sum = 0;
+
+        if (i >= 0)
+        {
+            sum += coefficiants[i];
+            --i;
+        }
+
+        if (j >= 0)
+        {
+            sum += other.coefficiants[j];
+            --j;
+        }
+
+        result.push(sum);
+    }
+
+    vector<int> resultVector;
+    while (!result.empty())
+    {
+        resultVector.push_back(result.top());
+        result.pop();
+    }
+
+    return HunPolynomial(resultVector);
+}
+
+HunPolynomial HunPolynomial::operator-(const HunPolynomial& other) const
+{
+    stack<int> result;
+    int i = coefficiants.size() - 1, j = other.coefficiants.size() - 1;
+
+    while (i >= 0 || j >= 0)
+    {
+        int difference = 0;
+
+        if (i >= 0)
+        {
+            difference += coefficiants[i];
+            --i;
+        }
+
+        if (j >= 0)
+        {
+            difference -= other.coefficiants[j];
+            --j;
+        }
+
+        result.push(difference);
+    }
+
+    vector<int> resultVector;
+    while (!result.empty())
+    {
+        resultVector.push_back(result.top());
+        result.pop();
+    }
+
+    return HunPolynomial(resultVector);
+}
+
+HunPolynomial HunPolynomial::operator*(const HunPolynomial& other) const
+{
+    vector<int> result(((coefficiants.size() - 1) + (other.coefficiants.size() - 1)) + 1, 0);
+
+    for (int i = 0; i < coefficiants.size(); ++i)
+    {
+        unsigned int power1 = (coefficiants.size() - 1) - i;
+
+        for (int j = 0; j < other.coefficiants.size(); ++j)
+        {
+            unsigned int power2 = (other.coefficiants.size() - 1) - j;
+            unsigned int power = (result.size() - 1) - (power1 + power2);
+
+            result[power] += coefficiants[i] * other.coefficiants[j];
+        }
+    }
+
+    return HunPolynomial(result);
+}
+
+ostream& operator<<(ostream& out, const HunPolynomial& polynomial)
+{
+    if (polynomial.coefficiants.size() == 1 && polynomial.coefficiants[0] != 0)
+        out << polynomial.coefficiants[0];
+
+    for (int i = 0; i < polynomial.coefficiants.size(); ++i)
+    {
+        int coefficiant = polynomial.coefficiants[i];
+        if (coefficiant == 0)
+            continue;
+        if (i == 0)
+        {
+            if (coefficiant > 0 && coefficiant != 1)
+                out << coefficiant << "*";
+            else if (coefficiant < 0 && coefficiant != -1)
+                out << "- " << -coefficiant << "*";
+            else if (coefficiant == -1)
+                out << "- ";
+        }
+        else if (i > 0 && i < polynomial.coefficiants.size() - 1)
+        {
+            if (coefficiant > 0 && coefficiant != 1)
+                out << " + " << coefficiant << "*";
+            else if (coefficiant < 0 && coefficiant != -1)
+                out << " - " << -coefficiant << "*";
+            else if (coefficiant == 1)
+                out << " + ";
+            else if (coefficiant == -1)
+                out << " - ";
+        }
+        else if (i == polynomial.coefficiants.size() - 1)
+        {
+            if (coefficiant > 0)
+                out << " + " << coefficiant;
+            else if (coefficiant < 0)
+                out << " - " << -coefficiant;
+        }
+
+        if (i < polynomial.coefficiants.size() - 2)
+            out << "x^" << (polynomial.coefficiants.size() - 1 - i);
+        else if (i == polynomial.coefficiants.size() - 2)
+            out << "x";
+    }
+
+    return out;
+}
+
+float HunPolynomial::operator()(const float& x) const
+{
+    float result = 0;
+
+    for (int i = 0; i < coefficiants.size(); ++i)
+        result += coefficiants[i] * pow(x, coefficiants.size() - i - 1);
+
+    return result;
+}
---
+++ Project 4/HunPolynomial.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include<cmath>
+#include <vector>
+#include <stack>
+using std::ostream, std::vector, std::stack;
+
+
+class HunPolynomial
+{
+private:    
+    vector<int> coefficiants;
+
+public:
+    HunPolynomial();
+    HunPolynomial(const vector<int>& coefficiants);
+
+    void Set(const vector<int>& coefficiants);
+    HunPolynomial operator+(const HunPolynomial& other) const;
+    HunPolynomial operator-(const HunPolynomial& other) const;
+    HunPolynomial operator*(const HunPolynomial& other) const;
+    friend ostream& operator<<(ostream& out, const HunPolynomial& polynomial);
+    float operator()(const float& x) const;
+};
